ae2a324c896ad4efe423ed88aeadb0ab0343aea1
Jesús Alexander MM <wiseowldev04@gmail.com>
2025-04-19 15:15:49 -0600

    Interfaces de inicio

--- lib/config/auth/auth_provider.dart
+++
@@ -1,8 +0,0 @@
-// lib/config/auth/auth_provider.dart
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-// Reexportar el provider de autenticación para facilitar su uso
-export '../../features/auth/presentation/providers/auth_provider.dart';
-
-// Este archivo sirve como punto central para obtener el estado de autenticación
-// Simplifica las importaciones al usar el router
